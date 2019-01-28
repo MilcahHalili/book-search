4c77e6d061d09526a2f96b1b02ac023265061bf2
MilcahHalili <halilicenteno@gmail.com>
2019-01-27 22:50:20 -0800

    create cached element references

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// cached element references
+const search = document.getElementById('search');
+const button = document.getElementById('button');
+const results = document.getElementById('results');

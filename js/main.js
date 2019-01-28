833e86e0b03a96c4a5353367155f54e4618fb735
MilcahHalili <halilicenteno@gmail.com>
2019-01-27 22:52:55 -0800

    create submit on enter event listner and function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 const search = document.getElementById('search');
 const button = document.getElementById('button');
 const results = document.getElementById('results');
+
+// event listeners
+search.addEventListener('keyup', searchOnEnter);
+
+// functions
+function searchOnEnter(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    button.click();
+  }
+};
